d66787568d23460d3a8489f72dbb845dc9e1f904
Chinmay Swami <chinmayasami@gmail.com>
2021-01-24 13:06:45 -0500

    removed stabndard scalar code because manual scaling and standard scalar gave same results

--- trigno_demo.py
+++ trigno_demo.py
@@ -11,7 +11,7 @@ from sklearn.preprocessing import StandardScaler
 def create_standard_scalar():
     scalar = StandardScaler()
     scalar.mean_ = np.array([804.68])
-    scalar.var_ = np.array([32228.98001])
+    scalar.var_ = np.array([67047282117])
     scalar.scale_ = np.array(np.sqrt(scalar.var_))
     return scalar
 
@@ -32,8 +32,7 @@ def smooth_data(data, zi):
 
 
 def check_accel(host):
-    dev = pytrigno.TrignoAccel(channel_range=(0, 35), samples_per_read=1,
-                               host=host)
+    dev = pytrigno.TrignoAccel(channel_range=(0, 35), samples_per_read=1, host=host)
     dev.check_sensor_n_type(1)
     dev.check_sensor_n_mode(1)
     dev.check_sensor_n_start_index(1)
@@ -55,9 +54,13 @@ def normalize_data(data):
     return n_data
 
 
+def scale_data(data):
+    n_data = (data - min) / (max - min)
+    return n_data
+
+
 def create_connection_accel(host):
-    dev = pytrigno.TrignoAccel(channel_range=(0, 35), samples_per_read=1,
-                               host=host)
+    dev = pytrigno.TrignoAccel(channel_range=(0, 35), samples_per_read=1, host=host)
     dev.check_sensor_n_type(1)
     dev.check_sensor_n_mode(1)
     dev.check_sensor_n_start_index(1)
@@ -80,7 +83,7 @@ def check_accel_thread(host):
         zs.append(data[2][0])
 
 
-def check_accel_thread_filtered(host, scalar):
+def check_accel_thread_filtered(host):
     dev.check_sensor_n_type(1)
     dev.check_sensor_n_mode(1)
     dev.check_sensor_n_start_index(1)
@@ -91,11 +94,11 @@ def check_accel_thread_filtered(host, scalar):
     while True:
         data = dev.read()
         filtered = smooth_data(data[12][0], zi)
-        # norm = normalize_data(data[12][0])
-        norm = scalar.transform(np.array([data[12]]))
+        norm = normalize_data(data[12][0])
+        scaled = scale_data(data[12][0])
         xs.append(data[12][0])
-        ys.append(filtered)
-        zs.append(norm)
+        ys.append(norm)
+        zs.append(scaled)
 
 
 def animate(i):
@@ -124,12 +127,18 @@ zs = []
 sampling_frequency = 2000
 filterOrderIMU = 1
 lowCutoff = 1
-avg_mean_training = 1200.68
-avg_std_training = 1000.0515
+avg_mean_training = -0.56184
+avg_std_training = 46.0059
+max = 3.55477
+min = -2.8562
 
 b, zi = create_butterworth_filter()
 dev = create_connection_accel('localhost')
-scalar = create_standard_scalar()
+
+# standard_scalar = StandardScaler().fit(np.array([[0]]))
+# standard_scalar.mean_ = np.array([804.68])
+# standard_scalar.var_ = np.array([67047282117])
+# standard_scalar.scale_ = np.array(np.sqrt(67047282117))
 
 ax = fig.add_subplot(3, 1, 1)
 ay = fig.add_subplot(3, 1, 2)
@@ -137,7 +146,7 @@ az = fig.add_subplot(3, 1, 3)
 
 try:
     # acquire_data_thread = threading.Thread(target=check_accel_thread, args=('localhost', ))
-    acquire_data_thread = threading.Thread(target=check_accel_thread_filtered, args=('localhost', scalar, ))
+    acquire_data_thread = threading.Thread(target=check_accel_thread_filtered, args=('localhost', ))
 
     acquire_data_thread.daemon = True
     acquire_data_thread.start()
